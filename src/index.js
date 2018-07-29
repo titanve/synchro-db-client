32fd1343e60d3a845e01c0d756d8e5f3018e117e
Rafael Mora <rafael.mora.guti@gmail.com>
2018-07-29 18:28:03 -0500

    app layout and eslint config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "@atlaskit/css-reset";
 import styled from "styled-components";
 
+const Br = styled.div`
+    width: 100%;
+`
+
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
@@ -10,9 +14,37 @@ const Container = styled.div`
   padding: 8px;
 
   display: flex;
+  flex-flow: row wrap;
+  align-content: center;
   justify-content: center;
-  align-items: center;
 `;
+
+const ProgramacionContainer = styled.div`
+margin: 8px;
+border: 1px solid lightgrey;
+border-radius: 2px;
+padding: 8px;
+`
+
+const ProgramacionesContainer = styled.div`
+display: flex;
+  align-content: center;
+  justify-content: center;
+`
+
+const ProgramacionesActivasContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  `
+
+const ProgramacionesInactivasContainer = styled.div`
+margin: 8px;
+border: 1px solid lightgrey;
+border-radius: 2px;
+padding: 8px;
+`
 
 class App extends React.Component {
   render() {
@@ -20,6 +52,19 @@ class App extends React.Component {
       <React.Fragment>
         <Container>
           <h1>Synchro db app</h1>
+          <Br/>
+          <ProgramacionContainer>
+              <b>Nueva/Editar Programacion</b>
+          </ProgramacionContainer>
+          <Br/>
+          <ProgramacionesContainer>
+            <ProgramacionesInactivasContainer>
+                <b>Programaciones inactivas</b>
+            </ProgramacionesInactivasContainer>
+            <ProgramacionesActivasContainer>
+                <b>Programaciones activas</b>
+            </ProgramacionesActivasContainer>
+          </ProgramacionesContainer>
         </Container>
       </React.Fragment>
     );

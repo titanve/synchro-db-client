25445b2a340a773719e75512629f085dd2ffa710
Rafael Mora <rafael.mora.guti@gmail.com>
2018-07-29 17:57:49 -0500

    styling the container component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom";
 import "@atlaskit/css-reset";
 import styled from "styled-components";
 
 const Container = styled.div`
-margin: 8px;
-border: 1px solid lightgrey;
-border-radius: 2px;
-`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 class App extends React.Component {
-    render(){
-        return (
-            <React.Fragment>
-                <Container>
-                <h1>Synchro db app</h1>
-                </Container>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <h1>Synchro db app</h1>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));

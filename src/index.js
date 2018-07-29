d2030ce63a30052b478d0d7afc06922808fecc13
Rafael Mora <rafael.mora.guti@gmail.com>
2018-07-29 16:56:23 -0500

    testin eslint and prettier

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 import "@atlaskit/css-reset";
 import styled from "styled-components";
 
+const Container = styled.div`
+margin: 8px;
+border: 1px solid lightgrey;
+border-radius: 2px;
+
+`
+
 class App extends React.Component {
     render(){
         return (
             <Reac.Fragment>
+                <Container>
                 <h1>Synchro db app</h1>
+                </Container>
             </Reac.Fragment>
         )
     }

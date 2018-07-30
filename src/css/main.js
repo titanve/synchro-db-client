c2335e7f175808b03ed6a9d9d07ab5489c18f1dd
Rafael Mora <rafael.mora.guti@gmail.com>
2018-07-29 19:03:56 -0500

    app layout, created a main css file

---
+++ src/css/main.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Br = styled.div`
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+
+  display: flex;
+  flex-flow: row wrap;
+  align-content: center;
+  justify-content: center;
+`;
+
+export const ProgramacionContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+`;
+
+export const ProgramacionesContainer = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+`;
+
+export const ProgramacionesActivasContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+`;
+
+export const ProgramacionesInactivasContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+`;
+
+export const NotificacionesContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+`;
+
+export const ControlPanelContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+`;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "@atlaskit/css-reset";
-import styled from "styled-components";
-
-const Br = styled.div`
-    width: 100%;
-`
-
-const Container = styled.div`
-  margin: 8px;
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-  padding: 8px;
-
-  display: flex;
-  flex-flow: row wrap;
-  align-content: center;
-  justify-content: center;
-`;
-
-const ProgramacionContainer = styled.div`
-margin: 8px;
-border: 1px solid lightgrey;
-border-radius: 2px;
-padding: 8px;
-`
-
-const ProgramacionesContainer = styled.div`
-display: flex;
-  align-content: center;
-  justify-content: center;
-`
-
-const ProgramacionesActivasContainer = styled.div`
-  margin: 8px;
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-  padding: 8px;
-  `
-
-const ProgramacionesInactivasContainer = styled.div`
-margin: 8px;
-border: 1px solid lightgrey;
-border-radius: 2px;
-padding: 8px;
-`
+import {
+  Container,
+  Br,
+  ProgramacionContainer,
+  ProgramacionesActivasContainer,
+  ProgramacionesInactivasContainer,
+  ProgramacionesContainer,
+  NotificacionesContainer,
+  ControlPanelContainer
+} from "./css/main.js";
 
 class App extends React.Component {
   render() {
@@ -52,19 +18,27 @@ class App extends React.Component {
       <React.Fragment>
         <Container>
           <h1>Synchro db app</h1>
-          <Br/>
+          <Br />
+          <NotificacionesContainer>
+            <b>Notificaciones</b>
+          </NotificacionesContainer>
+          <Br />
           <ProgramacionContainer>
-              <b>Nueva/Editar Programacion</b>
+            <b>Nueva/Editar Programacion</b>
           </ProgramacionContainer>
-          <Br/>
+          <Br />
           <ProgramacionesContainer>
             <ProgramacionesInactivasContainer>
-                <b>Programaciones inactivas</b>
+              <b>Programaciones inactivas</b>
             </ProgramacionesInactivasContainer>
             <ProgramacionesActivasContainer>
-                <b>Programaciones activas</b>
+              <b>Programaciones activas</b>
             </ProgramacionesActivasContainer>
           </ProgramacionesContainer>
+          <Br />
+          <ControlPanelContainer>
+            <b>Control panel</b>
+          </ControlPanelContainer>
         </Container>
       </React.Fragment>
     );
